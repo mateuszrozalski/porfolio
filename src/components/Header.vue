bb3a7a9ca8bf0551378f2d49b8e3b7599716ecbd
Mateusz Różalski <olivetum@gmail.com>
2017-11-14 20:09:22 +0100

    change font to pt-serif

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,8 @@
 <div>
   <div style="position: absolute; top: 0; left: 0;">
     <div class="container space-between hello justify-txt">
-      <h1 class="barlow-condensed site-name">{{name}}<br class="mobile-only"><span class="txt4 serif" style="font-weight: 400; font-size: 120%;"> Design</span></h1>
-      <p class="serif desktop-only txt5">
-        <!-- <strong></strong><br /> -->
+      <h1 class="barlow-semi-condensed site-name">{{name}}<br class="mobile-only"><span class="txt4 serif" style="font-weight: 400; font-size: 100%;"> Design</span></h1>
+      <p class="serif desktop-only txt5" style="line-height: 240%; font-size: 100%">
         branding, web development, web design, print design...
       </p>
     </div>
