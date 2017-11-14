e6e5e5de630412062ff9c28cc6ecf60fe7ea99d5
Mateusz Różalski <olivetum@gmail.com>
2017-11-14 13:25:48 +0100

    change header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,9 @@
   <div style="position: absolute; top: 0; left: 0;">
     <div class="container space-between hello justify-txt">
       <h1 class="barlow-condensed site-name">{{name}}<br class="mobile-only"><span class="txt4"> DESIGN</span></h1>
-      <p class="serif desktop-only">
+      <p class="serif desktop-only txt4">
         <!-- <strong></strong><br /> -->
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        branding, web development, web design, print design
       </p>
     </div>
   </div>
