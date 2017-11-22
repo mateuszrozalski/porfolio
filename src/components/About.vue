6dbc95c4bca807525aa6ab700ff968c957c84b0d
Mateusz Różalski <olivetum@gmail.com>
2017-11-22 10:54:26 +0100

    add contact links

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,7 +7,10 @@
          <div class="work-bg" ></div>
          <div class="justify-txt" style="position: relative; padding: 0 40px;">
              <h3 class="site-name barlow-condensed txt4">Pleased to meet you.</h3>
-             <p class="serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. laborum.</p>
+             <p class="serif">I am a graphic designer and web developer working and leaving in Warsaw. If You like my work contact me and tell me how I can help in your project.</p>
+             <a class="barlow-condensed txt1" href="mailto:olivetum@gmail.com"><strong>Mail</strong></a><br>
+             <a class="barlow-condensed txt5" href="tel:+48531051434"><strong>Phone</strong></a>
+
            </div>
        </div>
      </div>
